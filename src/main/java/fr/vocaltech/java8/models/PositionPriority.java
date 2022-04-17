c252b398d1a914c9f563edc1a8f4fd5e86f7882d
vocaltech <it@vocaltech.fr>
2022-04-17 21:15:32 +0200

    Implement Comparable interface

--- src/main/java/fr/vocaltech/java8/models/PositionPriority.java
+++ src/main/java/fr/vocaltech/java8/models/PositionPriority.java
@@ -3,13 +3,41 @@ package fr.vocaltech.java8.models;
 import lombok.Data;
 import lombok.ToString;
 
+import java.util.Objects;
+
 @Data
-@ToString
-public class PositionPriority extends Position {
+@ToString(callSuper = true)
+public class PositionPriority extends Position
+        implements Comparable<PositionPriority> {
     private byte priority = 0;
 
     public PositionPriority(byte priority, double latitude, double longitude, long time, String trackId, String userId) {
         super(latitude, longitude, time, trackId, userId);
         this.priority = priority;
+    }
+
+    @Override
+    public int compareTo(PositionPriority o) {
+        if (priority > o.priority) {
+            return 1;
+        } else if (priority < o.priority) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        PositionPriority that = (PositionPriority) o;
+        return priority == that.priority;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), priority);
     }
 }
