94cf39624649804126a8774f0202734eb6d5c064
vocaltech <it@vocaltech.fr>
2022-04-18 12:08:28 +0200

    Initial commit

---
+++ src/test/java/fr/vocaltech/java8/streams/RuleMatrixTest.java
@@ -0,0 +1,15 @@
+package fr.vocaltech.java8.streams;
+
+import fr.vocaltech.java8.models.Cart;
+
+import lombok.var;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+class RuleMatrixTest {
+    private Cart cart;
+
+
+
+}
