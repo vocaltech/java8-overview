7fe2fe9c1a3ddbac80d6a3c33e0963f4c8b66d73
vocaltech <it@vocaltech.fr>
2022-04-18 11:14:26 +0200

    Initial commit

---
+++ src/main/java/fr/vocaltech/java8/models/Cart.java
@@ -0,0 +1,13 @@
+package fr.vocaltech.java8.models;
+
+public class Cart {
+    private double value;
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+}
