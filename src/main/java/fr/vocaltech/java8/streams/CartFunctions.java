8c2c32cdd0f9363387df3a59f6492b6556f3bcdd
vocaltech <it@vocaltech.fr>
2022-04-18 12:05:14 +0200

    Implement functions class

--- src/main/java/fr/vocaltech/java8/streams/CartFunctions.java
+++ src/main/java/fr/vocaltech/java8/streams/CartFunctions.java
@@ -1,4 +1,11 @@
 package fr.vocaltech.java8.streams;
 
+import fr.vocaltech.java8.models.Cart;
+
+import java.util.function.Function;
+
 public class CartFunctions {
+    public static final Function<Cart, Double> MULTIPLY_WITH_ONE_POINT_FIVE = cart -> cart.getValue() * 1.5;
+    public static final Function<Cart, Double> MULTIPLY_WITH_TWO_POINT_FIVE = cart -> cart.getValue() * 2.5;
+    public static final Function<Cart, Double> DEFAULT = cart -> 1.5;
 }
