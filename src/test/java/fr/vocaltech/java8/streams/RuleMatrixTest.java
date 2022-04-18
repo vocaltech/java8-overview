bf4b6fa89186fdee4fee70bb7585dc88e5f55d27
vocaltech <it@vocaltech.fr>
2022-04-18 12:10:34 +0200

    Add test for cartValue = 6

--- src/test/java/fr/vocaltech/java8/streams/RuleMatrixTest.java
+++ src/test/java/fr/vocaltech/java8/streams/RuleMatrixTest.java
@@ -10,6 +10,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 class RuleMatrixTest {
     private Cart cart;
 
+    @Test
+    void givenCartValueSix_thenReturn_Six_Multiply_1_5() {
+        cart = new Cart();
+        cart.setValue(6.0);
+
+        var result = RuleMatrix.getRule(cart).apply(cart);
+        assertThat(result).isEqualTo(9.0);
+    }
 
 
 }
