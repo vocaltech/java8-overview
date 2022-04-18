76802698dfadec7551585156739f44cdaca8c49c
vocaltech <it@vocaltech.fr>
2022-04-18 12:05:37 +0200

    Implement predicates class

--- src/main/java/fr/vocaltech/java8/streams/CartPredicates.java
+++ src/main/java/fr/vocaltech/java8/streams/CartPredicates.java
@@ -1,4 +1,10 @@
 package fr.vocaltech.java8.streams;
 
+import fr.vocaltech.java8.models.Cart;
+
+import java.util.function.Predicate;
+
 public class CartPredicates {
+    public static final Predicate<Cart> WHEN_CART_VALUE_IS_SIX = cart -> cart.getValue() == 6;
+    public static final Predicate<Cart> WHEN_CART_VALUE_IS_SEVEN = cart -> cart.getValue() == 7;
 }
