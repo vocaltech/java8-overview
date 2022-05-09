b44369b7734b75172da988c16ce5178d1c1a2b56
vocaltech <it@vocaltech.fr>
2022-05-09 22:56:28 +0200

    Add givenList_whenAddOneElement_thenSizeIsOne test

--- src/test/java/fr/vocaltech/java8/streams/StreamsTest.java
+++ src/test/java/fr/vocaltech/java8/streams/StreamsTest.java
@@ -3,16 +3,25 @@ package fr.vocaltech.java8.streams;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class StreamsTest {
-    @Test
-    void givenList_whenCreateStream_returnStream() throws ClassNotFoundException {
-        List<String> list = new ArrayList<>();
-        list.add("a");
+    List<String> list;
 
+    @BeforeEach
+    void beforeEach() {
+        list = new ArrayList<>();
+        list.add("elt1");
+    }
+    @Test
+    void givenList_whenCreateStream_thenReturnStream() throws ClassNotFoundException {
         assertThat(list.stream()).isInstanceOf(Class.forName("java.util.stream.Stream"));
     }
 
+    @Test
+    void givenList_whenAddOneElement_thenSizeIsOne() {
+        assertThat(list.stream().distinct().count()).isEqualTo(1);
+    }
 }
