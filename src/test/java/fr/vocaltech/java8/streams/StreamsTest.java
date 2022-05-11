5c8e5154426d23f8700691ee17ac70680d24690e
vocaltech <it@vocaltech.fr>
2022-05-11 23:49:34 +0200

    Cleaning imports

--- src/test/java/fr/vocaltech/java8/streams/StreamsTest.java
+++ src/test/java/fr/vocaltech/java8/streams/StreamsTest.java
@@ -2,7 +2,6 @@ package fr.vocaltech.java8.streams;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
